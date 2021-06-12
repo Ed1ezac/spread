905e5d9b4ae1c326d3fe7ed571b2eb7b9ea1c2bc
Ed1ezac <ed1ezac@gmail.com>
2021-06-12 02:37:37 +0200

    adds additional funds processing functionality

--- app/Models/JobStatus.php
+++ app/Models/JobStatus.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
 
 class JobStatus extends Model
@@ -21,8 +21,19 @@ class JobStatus extends Model
     public $dates = ['started_at', 'finished_at', 'created_at', 'updated_at'];
 
     public function user(){
-        
         return $this->belongsTo(User::class);
+    }
+
+    public function scopeMine($query){
+        return $query->where('user_id', Auth::id());
+    }
+
+    public function scopeWithStatus($query, $status){
+        return $query->where('status', $status);
+    }
+
+    public function scopeForJob($query, $jobId){
+        return $query->where('job_id', $jobId);
     }
 
     public function markAsExecuting(){
