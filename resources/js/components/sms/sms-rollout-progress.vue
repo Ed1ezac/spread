76d6790cfe04c4c01134701ea5d2ce651b1f09d6
Ed1ezac <ed1ezac@gmail.com>
2021-06-20 13:17:57 +0200

    refactors template

--- resources/js/components/sms/sms-rollout-progress.vue
+++ resources/js/components/sms/sms-rollout-progress.vue
@@ -7,7 +7,7 @@
                     <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
-                    <div v-if="!isSending" class="animate-pulse bg-gray-300 ml-2 rounded h-4 w-16"></div>
+                    <div v-if="!isDiscovered" class="animate-pulse bg-gray-300 ml-2 rounded h-4 w-16"></div>
                     <h3 v-else class="text-gray-500 ml-2 text-sm font-bold">{{recipientsName}}</h3>
                 </div>
             </div>
@@ -31,7 +31,7 @@
                 <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
-                <div v-if="!isSending" class="animate-pulse bg-gray-300 ml-4 rounded h-4 w-16"></div>
+                <div v-if="!isDiscovered" class="animate-pulse bg-gray-300 ml-4 rounded h-4 w-16"></div>
                 <h3 v-else class="text-sm font-bold ml-4 pt-1 text-gray-600">{{smsSender}}</h3>
             </div>
             <div class="flex h-20 md:h-14">                
@@ -39,7 +39,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
                 </svg>
                 <div class="bg-gray-200 ml-4 px-2 p-1 w-full border-gray-400 border-2">
-                    <div v-if="!isSending" class="animate-pulse">
+                    <div v-if="!isDiscovered" class="animate-pulse">
                         <div class="flex-1 space-y-2">
                             <div class="h-4 bg-gray-300 rounded w-5/6"></div>
                             <div class="h-4 bg-gray-300 rounded w-4/6"></div>
@@ -52,7 +52,7 @@
                 <!--p v-else v-if="recipients != null" class="text-gray-500 ml-2 pt-1 text-base font-medium">{{ recipients.name }}</!--p -->
                 <p v-if="false" class="bg-red-100 rounded text-red-400 ml-2 p-1 pt-1 text-base font-medium">missing recipients, sending will fail!</p>
             </div>
-            <div v-if="smsId != null && percentage < 30" class="flex justify-end">
+            <div v-if="canAbort" class="flex justify-end">
                 <span class="sm:block">
                     <button @click="abortRollout()" class="inline-flex justify-center py-2 px-4 my-btn shadow-md bg-primary-500 border-primary-500 hover:bg-primary-700 focus:ring-primary-800">
                         <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -96,8 +96,7 @@ export default {
             style.width = this.percentage + '%'
             return style
         },
-        statusValue(){
-            //discovering, Sending, Complete
+        statusValue(){            
             if(this.isSending && !this.isAborted && !this.isCompleted){
                 return 'Sending...';
             }else if(this.isCompleted){
@@ -111,12 +110,17 @@ export default {
             return this.smsId != undefined && this.percentage < 100;
         },
         isAborted(){
-            return (this.expectedMax > 0) && 
-                    (this.current != this.total) &&
-                    (this.current == this.expectedMax);
+            return ((this.expectedMax > 0)&&(this.current === this.expectedMax));
+        },
+        canAbort(){
+            return !this.isAborted && this.isDiscovered && 
+                (this.total >= 10000 ? this.current < 1500 : this.percentage < 15);
         },
         isCompleted(){
             return this.percentage >= 100;
+        },
+        isDiscovered(){
+            return this.smsId != undefined;
         }
     },
     methods :{
@@ -128,7 +132,7 @@ export default {
     },
     mounted () {
         if(this.percentage < 100){
-            Echo.private(`rollouts.${this.userId}`)//(`uploads.${this.userId}`)
+            Echo.private(`rollouts.${this.userId}`)
             .listen('ReportProgress', (e) => {
                 this.smsId = e.smsId;
                 this.total = e.total;
