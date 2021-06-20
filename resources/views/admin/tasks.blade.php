70e614785d2908fc9cf0f51c586d2b79ff7c0a00
Ed1ezac <ed1ezac@gmail.com>
2021-06-20 13:03:04 +0200

    adds more admin stuff

--- resources/views/admin/tasks.blade.php
+++ resources/views/admin/tasks.blade.php
@@ -1,8 +1,106 @@
 @extends('layouts.admin-header')
 
 @section('features')
-    <h3 class="mt-24 ml-24">Sms Rollout Tasks</h3>
-@endsection
-@push('page-js')
+    
+    <div>
+    
+    @if(isset($history) && count($history)>0)
+    <div class="mr-4 xl:mr-6 mt-8 xl:mt-10 mb-8">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="max-w-7xl shadow overflow-hidden border-b border-gray-200 sm:rounded">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-headings font-bold text-gray-500 uppercase tracking-wider">
+                            User
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-headings font-bold text-gray-500 uppercase tracking-wider">
+                            Status
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-headings font-bold text-gray-500 uppercase tracking-wider">
+                            Progress
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-headings font-bold text-gray-500 uppercase tracking-wider">
+                            Start Time
+                            </th>
+                            <th scope="col" class="px-6 py-3 text-left text-xs font-headings font-bold text-gray-500 uppercase tracking-wider">
+                            Total Time
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white divide-y divide-gray-200">
+                    @foreach ($history as $record)
+                        <tr>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="flex items-center">
+                                    <div class="w-10 h-10 flex-shrink-0">
+                                        <div class="w-full h-full bg-gray-700 rounded-full overflow-hidden shadow-inner text-center bg-purple table">
+                                            <span class="table-cell text-sm text-white font-bold align-middle">{{ (\App\Models\User::find($record->user_id))->initials }}</span>
+                                        </div>
+                                    </div>
+                                    <div class="ml-4">
+                                        <div class="text-sm font-medium text-gray-900">
+                                        {{ (\App\Models\User::find($record->user_id))->name }}
+                                        </div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="flex items-center">
+                                @if($record->status == 'failed')
+                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-200 text-red-800">
+                                    {{$record->status}}
+                               </span>
+                               @elseif($record->status == 'finished')
+                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                    {{$record->status}}
+                               </span>
+                               @elseif($record->status == 'executing' || $record->status == 'retrying')
+                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-primary-200 text-primary-800">
+                                    {{$record->status}}
+                               </span>
+                               @elseif($record->status == 'queued')
+                                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-primary-200 text-primary-800">
+                                    {{$record->status}}
+                               </span>
+                               @endif
+                            </div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm font-bold text-gray-800">{{$record->progress_now.' \\ '.$record->progress_max}}</div>
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                @if(isset($record->started_at) && $record->started_at->diffInDays() < 1 )
+                                <div class="text-sm text-gray-500">
+                                    {{ $record->started_at->diffForHumans() }}
+                                </div>
+                                @elseif(isset($record->started_at))
+                                <div class="text-sm text-gray-500">
+                                    {{ $record->started_at->toDayDateTimeString() }}
+                                </div>
+                                @else
+                                <div class="text-sm text-gray-500">
+                                    --:--
+                                </div>
+                                @endif
+                            </td>
+                            <td class="px-6 py-4 whitespace-nowrap">
+                                <div class="text-sm text-gray-500">
+                                {{ ($record->finished_at == null || $record->started_at == null) ? 'pending..' : 
+                                    $record->started_at->diffForHumans($record->finished_at, true) }}
+                                </div>                                
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                <div class="px-4 py-2">{{ $history->links() }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+    </div>
 
-@endpush
+@endsection
--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -32,14 +32,17 @@
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <div class="flex items-center">
                                 <div class="flex-shrink-0 h-10 w-10">
-                                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt="">
+                                    <!--img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60" alt=""-->
+                                    <div class="w-full h-full bg-gray-700 rounded-full overflow-hidden shadow-inner text-center bg-purple table">
+                                        <span class="table-cell text-sm text-white font-bold align-middle">{{ $user->initials }}</span>
+                                    </div>
                                 </div>
                                 <div class="ml-4">
                                     <div class="text-sm font-medium text-gray-900">
-                                    {{$user->name}}
+                                    {{ $user->name }}
                                     </div>
                                     <div class="text-sm text-gray-500">
-                                    {{$user->email}}
+                                    {{ $user->email }}
                                     </div>
                                 </div>
                                 </div>
@@ -62,6 +65,7 @@
                         @endforeach
                     </tbody>
                 </table>
+                <div class="px-4 py-2">{{ $users->links() }}</div>
             </div>
         </div>
     </div>
