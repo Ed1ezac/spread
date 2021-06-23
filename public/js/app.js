5bd5171fe99062eba5db3545be864cd2c37977df
Ed1ezac <ed1ezac@gmail.com>
2021-06-24 00:24:03 +0200

    proper web server config for production...

--- public/js/app.js
+++ public/js/app.js
@@ -26500,7 +26500,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__.default({
   broadcaster: 'pusher',
-  key: "dummy",
+  key: "spread-app",
   cluster: "mt1",
   wsHost: window.location.hostname,
   wsPort: 6001,
