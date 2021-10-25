bcbf2fce62a7fa7b315069747d50efcf5041c608
Ed1ezac <ed1ezac@gmail.com>
2021-10-25 13:46:55 +0200

    refactors orange api functions

---
+++ app/Traits/CallsOrangeApi.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Traits;
+
+trait CallsOrangeApi{ 
+
+    /**
+     * cURL option for whether to verify the peer's certificate or not.
+     *
+     * @var bool
+     */
+    protected $verifyPeerSSL = true;
+
+    /**
+     *  Gets the CURLOPT_SSL_VERIFYPEER value.
+     * @return bool
+     */
+    public function getVerifyPeerSSL()
+    {
+        return $this->verifyPeerSSL;
+    }
+
+    /**
+     *  Calls API Endpoints.
+     * @param  array   $headers         An array of HTTP header fields to set
+     * @param  array   $args            The data to send
+     * @param  string  $url             The URL to fetch
+     * @param  string  $method          Whether to do a HTTP POST or a HTTP GET
+     * @param  int     $successCode     The HTTP code that will be returned on
+     *                                  success
+     * @param  bool    $jsonEncodeArgs  Whether or not to json_encode $args
+     * @return array   Contains the results returned by the endpoint or an error
+     *                 message
+     */
+    public function callApi(
+        $headers,
+        $args,
+        $url,
+        $method,
+        $successCode,
+        $jsonEncodeArgs = false
+    ) {
+        $ch = curl_init();
+    
+        if ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, true);
+
+            if (!empty($args)) {
+                if ($jsonEncodeArgs === true) {
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($args));
+                } else {
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($args));
+                }
+            }
+        } else /* $method === 'GET' */ {
+            if (!empty($args)) {
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . http_build_query($args));
+            } else {
+                curl_setopt($ch, CURLOPT_URL, $url);
+            }
+        }
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        if ($this->getVerifyPeerSSL() === false) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
+        // Make sure we can access the response when we execute the call
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $data = curl_exec($ch);
+
+        if ($data === false) {
+            return array('error' => 'API call failed with cURL error: ' . curl_error($ch));
+        }
+
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+ 
+        curl_close($ch);
+
+        $response = json_decode($data, true);
+
+        $jsonErrorCode = json_last_error();
+        if ($jsonErrorCode !== JSON_ERROR_NONE) {
+            return array(
+                'error' => 'API response not well-formed (json error code: '
+                    . $jsonErrorCode . ')'
+            );
+        }
+
+        if ($httpCode !== $successCode) {
+            $errorMessage = '';
+
+            if (!empty($response['error_description'])) {
+                $errorMessage = $response['error_description'];
+            } elseif (!empty($response['error'])) {
+                $errorMessage = $response['error'];
+            } elseif (!empty($response['description'])) {
+                $errorMessage = $response['description'];
+            } elseif (!empty($response['message'])) {
+                $errorMessage = $response['message'];
+            } elseif (!empty($response['requestError']['serviceException'])) {
+                $errorMessage = $response['requestError']['serviceException']['text']
+                    . ' ' . $response['requestError']['serviceException']['variables'];
+            } elseif (!empty($response['requestError']['policyException'])) {
+                $errorMessage = $response['requestError']['policyException']['text']
+                    . ' ' . $response['requestError']['policyException']['variables'];
+            }
+
+            return array('error' => $errorMessage);
+        }
+        return $response;
+    }
+
+}
