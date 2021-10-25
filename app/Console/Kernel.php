0da9bbab08f8423f64266fe91508522aa7147d33
Ed1ezac <ed1ezac@gmail.com>
2021-10-25 13:37:54 +0200

    adds token pulling task and model

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Helpers\TokenPuller;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -24,7 +25,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        //->everyFiveMinutes();
+        $schedule->call(new TokenPuller)
+                ->everyFifteenMinutes()
+                ->between("05:30", "23:30");
     }
 
     /**
