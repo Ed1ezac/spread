4de18c994a4c55eb665c874053dd53fb8f45db9e
Ed1ezac <ed1ezac@gmail.com>
2021-11-23 14:20:38 +0200

    emailing minor fixes

--- app/Traits/SendsMail.php
+++ app/Traits/SendsMail.php
@@ -3,11 +3,10 @@
 namespace App\Traits;
 
 use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Auth;
 
 trait SendsMail{
 
-    protected function sendEmail($mail){
-        Mail::to(Auth::user())->send($mail);
+    protected function sendEmail($email, $mail){
+        Mail::to($email)->send($mail);
     }
 }
