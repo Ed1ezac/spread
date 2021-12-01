7f04120185a11ce86a440dad29a3b4f2e4b4b49c
Ed1ezac <ed1ezac@gmail.com>
2021-12-01 13:53:07 +0200

    separates footer from home page

---
+++ resources/views/components/footer-small.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-auto py-6 bg-transparent">
+    <div class="flex flex-row justify-center">
+        <div class="text-base font-light text-gray-600">© {{date('Y')}} {{ config('app.name') }}. All Rights Reserved </div>
+    </div>
+</footer>
