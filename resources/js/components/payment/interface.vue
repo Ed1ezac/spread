2d3e3b6edaf7d6750c045eec7f4af522f60ca9f4
Ed1ezac <ed1ezac@gmail.com>
2022-03-01 17:59:32 +0200

    future payment tabs

---
+++ resources/js/components/payment/interface.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <tabs :tabList="tabList">
+      <template v-slot:tabPanel-1> Bank payment </template>
+      <template v-slot:tabPanel-2> My Zaka payment</template>
+      <template v-slot:tabPanel-3> Orange Money payment</template>
+      <template v-slot:tabPanel-4> Smega Payment</template>
+    </tabs>
+  </div>
+</template>
+
+<script>
+import Tabs from "./tabs";
+export default {
+    components: {
+        Tabs,
+    },
+  
+    data() {
+        return {
+            tabList: ["Bank", "Orange Money"],// "Smega","My Zaka", 
+        };
+    },
+};
+</script>
