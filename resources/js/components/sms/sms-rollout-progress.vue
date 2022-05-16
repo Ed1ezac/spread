cfb94c0eea8ae22929f837f8e8e338904eb7b080
Ed1ezac <ed1ezac@gmail.com>
2022-05-16 19:17:10 +0200

    adds static sms, echo config, fixes

--- resources/js/components/sms/sms-rollout-progress.vue
+++ resources/js/components/sms/sms-rollout-progress.vue
@@ -1,6 +1,6 @@
 <template>
-<!------------isAboutToSend || isSending || isAborted------- --->
-    <div v-show="true" class="shadow-sm overflow-hidden divide-y divide-dashed divide-gray-300 rounded mb-10 mr-4 xl:mr-6 max-w-7xl border-t-4 border-gray-500">
+<!----------- ---------->
+    <div class="shadow-sm overflow-hidden divide-y divide-dashed divide-gray-300 rounded mb-10 mr-4 xl:mr-6 max-w-7xl border-t-4 border-accent-600">
         <div class="mb-2 px-4">
             <div class="flex justify-between my-2">
                 <h3 class="text-sm text-gray-400">{{ statusValue }}</h3>
@@ -8,19 +8,19 @@
                     <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
-                    <div v-if="!isDiscovered" class="animate-pulse bg-gray-300 ml-2 rounded h-4 w-16"></div>
-                    <h3 v-else class="text-gray-500 ml-2 text-sm font-bold">{{recipientsName}}</h3>
+                    <h3 class="text-gray-500 ml-2 text-sm font-bold">{{recipientsName}}</h3>
                 </div>
             </div>
             <div class="h-5 w-full shadow-inner bg-gray-200 rounded-full">
                 <div v-if="percentage>0" 
                     class="h-full px-1 rounded-full" 
-                    :class="isSending ? 'bg-accent-600': isAborted ? 'bg-red-600': 'bg-primary-600'"
+                    :class="isSending ? 'bg-accent-600': aborted ? 'bg-red-600': 'bg-primary-600'"
                     :style="barStyle">
                     <div class="text-right">
                         <h3 v-if="percentage > 4" class="text-white font-bold">{{ percentage }}%</h3>
                     </div>
                 </div>
+                <div v-else class="animate-pulse bg-gray-300 h-full w-full rounded-full"></div>
             </div>
             <div class="flex mt-1 font-bold px-1 justify-between">
                 <div class="flex">
@@ -35,21 +35,15 @@
                 <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                 </svg>
-                <div v-if="!isDiscovered" class="animate-pulse bg-gray-300 ml-4 rounded h-4 w-16"></div>
-                <h3 v-else class="text-sm font-bold ml-4 pt-1 text-gray-600">{{smsSender}}</h3>
+                <div v-if="false" class="animate-pulse bg-gray-300 ml-4 rounded h-4 w-16"></div>
+                <h3 v-else class="text-sm font-bold ml-4 pt-1 text-gray-600">{{sms.sender}}</h3>
             </div>
             <div class="flex h-20 md:h-14">                
                 <svg class="flex-shrink-0 h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
                 </svg>
-                <div class="bg-gray-200 ml-4 px-2 p-1 w-full border-gray-400 border-2">
-                    <div v-if="!isDiscovered" class="animate-pulse">
-                        <div class="flex-1 space-y-2">
-                            <div class="h-4 bg-gray-300 rounded w-5/6"></div>
-                            <div class="h-4 bg-gray-300 rounded w-4/6"></div>
-                        </div>
-                    </div>
-                    <p class="font-medium text-sm text-gray-600">{{smsMessage}}</p>
+                <div class="bg-gray-200 ml-4 px-2 p-1 w-full border-gray-400 border-2">                    
+                    <p class="font-medium text-sm text-gray-600">{{sms.message}}</p>
                 </div>
             </div>
             <div class="flex">
@@ -67,7 +61,7 @@
                 </span>
                 <form ref="abortform" action="/sms/rollout/abort" method="POST" class="hidden">
                     <input type="hidden" name="_token" :value="csrf">
-                    <input type="hidden" name="id" :value="smsId">
+                    <input type="hidden" name="id" :value="sms.id">
                 </form>
             </div>
         </div>
@@ -79,26 +73,19 @@ export default {
         return{
             total: undefined,
             current: undefined,
-            smsId: undefined,
             sendingRate: 0,
-            smsSender: '',
-            smsMessage: '',
-            recipientsName: '',
-            expectedMax: this.maxProgress,
+            aborted: false,
+            discovered: false,
             csrf: document.querySelector('meta[name="csrf-token"]').getAttribute('content')
         }
     },
     props:{
-        aborted:{
-            type:Boolean,
-            default: false,
+        recipientsName: {
+            type: String,
+            default: '',
         },
-        maxProgress:{
-            type:Number,
-            default: -1,
-        },
+        sms: Object,
         userId: String,
-        isAboutToSend: Boolean,
     },
     computed: {
         percentage(){
@@ -109,36 +96,35 @@ export default {
             style.width = this.percentage + '%'
             return style
         },
-        statusValue(){            
+        statusValue(){   
             if((this.isSending) && (!this.isCompleted)){
                 return 'Sending...';
             }else if(this.isCompleted){
                 return 'Complete.';
-            }else if(this.isAborted){
+            }else if(this.aborted){
                 return 'Stopped.'
+            }else{
+                return 'Queued...';
             }
-            return 'Queueing...';
         },
         isSending(){
-            return this.isDiscovered && this.percentage < 100 && !this.isAborted;
-        },
-        isAborted(){
-            return ((this.expectedMax > 0)&&(this.percentage < 50)&&(this.current >= this.expectedMax));
+            return this.isDiscovered && this.percentage < 100 && !this.aborted;
         },
         canAbort(){
             return !this.aborted && this.isDiscovered && 
-                (this.total >= 10000 ? this.current < 1500 : this.percentage < 15);
+                (this.total >= 15000 ? this.current < 4000 : this.percentage < 43);
         },
         isCompleted(){
             return this.percentage >= 100;
         },
         isDiscovered(){
-            return this.smsId != undefined;
+            return this.discovered;
         }
     },
     methods :{
         abortRollout: function(){
-            if(this.smsId != undefined){
+            if(this.sms.id != undefined){
+                this.aborted = true;
                 this.$refs.abortform.submit();
             }
         },
@@ -147,18 +133,23 @@ export default {
         if(this.percentage < 100){
             Echo.private(`rollouts.${this.userId}`)
             .listen('ReportProgress', (e) => {
-                this.smsId = e.smsId;
-                this.total = e.total;
-                this.current = e.current;
-                this.smsSender = e.smsSender;
-                this.smsMessage = e.smsMessage;
-                this.sendingRate = e.sendingRate;
-                this.recipientsName = e.smsRecipients;
+                if(this.sms.id === e.smsId){
+                    if(this.discovered){   
+                        this.current = e.current;
+                        this.sendingRate = e.sendingRate;
+                    }else{
+                        this.discovered = true;
+                        this.total = e.total;
+                        this.current = e.current;
+                        this.sendingRate = e.sendingRate;
+                        this.recipientsName = e.smsRecipients;
+                    }
+                }
             })
             .listen('RolloutComplete', (e) =>{
-                //this.expectedMax = e.sentSmsCount;
+                
             });
-        }  
+        } 
     },
 }
 </script>
