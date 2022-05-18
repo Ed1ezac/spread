a5f54af1e209f7fae09bfeb2d57a36b067c5f559
Ed1ezac <ed1ezac@gmail.com>
2022-05-18 15:54:59 +0200

    adds abort ability for admin

--- resources/views/admin/queue.blade.php
+++ resources/views/admin/queue.blade.php
@@ -11,19 +11,36 @@
         @if(isset($queue) && count($queue)>0)
             @foreach ($queue as  $item)
                 @if ($item->status == 'executing')
-                    <div class="bg-white shadow rounded h-24 w-full px-4 py-2">
-                        <div>
+                    <div class="bg-primary-100 shadow rounded h-24 w-full px-4 py-2">
+                        <div class="flex justify-between">
                             <div class="flex flex-col">
-                                <div class="flex">
+                                <div class="flex items-baseline">
                                     <div class="text-gray-900 font-medium text-lg">{{App\Models\User::find($item->user_id)->name}}</div>
-                                    <div class="text-gray-900 font-medium text-lg">{{App\Models\Sms::find($item->trackable_id)->order_no}}</div>
+                                    <div class="ml-2 text-gray-600 font-medium text-sm">{{App\Models\Sms::find($item->trackable_id)->order_no}}</div>
                                 </div>
                                 <div class="text-sm text-gray-500 font-medium">0 out of {{$item->progress_max}} recipients</div>
                             </div>
-                            <div class="">
-                                <div class="text-xs text-gray-500 font-semibold">EXECUTING</div>
-                                <div class="text-xs text-gray-500 font-medium">started  3 hours</div>
+                            <div class="flex items-center space-x-4">
+                                <div>
+                                    <div class="text-xs text-green-500 font-semibold tracking-wider">EXECUTING</div>
+                                    <div class="text-xs text-gray-500 font-medium">Began: begins: {{Carbon\Carbon::createFromTimestamp($item->available_at)->toDateTimeString()}}</div>
+                                </div>
+                                <div class="flex justify-end">
+                                    <form action="/sms/rollout/abort" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="id" value="{{$item->trackable_id}}">
+                                        <span class="sm:block">
+                                            <button type="submit" class="inline-flex justify-center py-2 px-4 my-btn shadow-md bg-primary-500 border-primary-500 hover:bg-primary-700 focus:ring-primary-800">
+                                                <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                                </svg>
+                                                ABORT
+                                            </button> 
+                                        </span>
+                                    </form>
+                                </div>
                             </div>
+                        </div>
                         </div>
                     </div>
                 @else
@@ -31,14 +48,30 @@
                         <div class="flex justify-between">
                             <div class="flex flex-col">
                                 <div class="flex items-baseline">
-                                        <div class="text-gray-900 font-medium text-lg">{{App\Models\User::find($item->user_id)->name}}</div>
-                                        <div class="ml-2 text-gray-600 font-medium text-sm">{{App\Models\Sms::find($item->trackable_id)->order_no}}</div>
-                                    </div>
-                                    <div class="text-sm text-gray-500 font-medium">0 out of {{$item->progress_max}} recipients</div>
+                                    <div class="text-gray-900 font-medium text-lg">{{App\Models\User::find($item->user_id)->name}}</div>
+                                    <div class="ml-2 text-gray-600 font-medium text-sm">{{App\Models\Sms::find($item->trackable_id)->order_no}}</div>
                                 </div>
-                            <div class="">
-                                <div class="text-xs text-gray-500 font-semibold">QUEUED</div>
-                                <div class="text-xs text-gray-500 font-medium">begins in  3 hours</div>
+                                <div class="text-sm text-gray-500 font-medium">0 out of {{$item->progress_max}} recipients</div>
+                            </div>
+                            <div class="flex items-center space-x-4">
+                                <div class="flex flex-col justify-center items-end">
+                                    <div class="text-xs text-gray-500 font-semibold tracking-wider">QUEUED</div>
+                                    <div class="text-xs text-gray-500 font-medium">begins: {{Carbon\Carbon::createFromTimestamp($item->available_at)->toDateTimeString()}}</div>
+                                </div>
+                                <div class="flex justify-end">
+                                    <form action="/sms/rollout/abort" method="POST">
+                                        @csrf
+                                        <input type="hidden" name="id" value="{{$item->trackable_id}}">
+                                        <span class="sm:block">
+                                            <button type="submit" class="inline-flex justify-center py-2 px-4 my-btn shadow-md bg-primary-500 border-primary-500 hover:bg-primary-700 focus:ring-primary-800">
+                                                <svg class="-ml-1 mr-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+                                                </svg>
+                                                ABORT
+                                            </button> 
+                                        </span>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div> 
